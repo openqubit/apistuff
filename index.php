2c45b5f7c4d5a1ef3727ae759db777af4c5b1298
openqubit <qubitsmith@gmail.com>
2016-08-19 17:49:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,8 @@ $data_string = '
   "receiptNumber": "rec123",
   "bankNumber": "ban123"
 }
+// create client data required and not trnasaction related data
+
 ';
 
 $endpoint = "https://197.234.237.253:8443/fineract-provider/api/v1/";
@@ -28,7 +30,7 @@ $method = "POST";
 $api_target = "clients";
 $extra_parameters = '';
 //$extra_parameters = "&command=close";
-$URL= $endpoint.$api_target."?tenantIdentifier=".$tenant.$extra_parameters;
+$URL= $endpoint.$api_target." 
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$URL);
@@ -37,7 +39,8 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
-    'Content-Length: ' . strlen($data_string))
+    'Content-Length: ' . strlen($data_string),
+    'Fineract-Platform-TenantId:default')  // << missing header
 );
 curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
